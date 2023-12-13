59847739b1ed2ac94ec9272ebd283532c42d81f9
nquinbla <nquinbla@myuax.com>
2023-12-13 22:31:24 +0100

    a

--- 1. Las funciones comunes del lenguaje C/main.cpp
+++ 1. Las funciones comunes del lenguaje C/main.cpp
@@ -1,9 +1,9 @@
 // a. Utiliza la función abs() para calcular el valor absoluto de un número entero. Prueba con varios números, incluyendo negativos.
 // b. Usa la función rand() para generar un número aleatorio. Luego, usa srand() para cambiar la semilla de la generación de números aleatorios y observa cómo cambian los números generados.
 
-#include <iostream>
-#include <stdlib.h>
-#include <stdio.h>
+#include <iostream> // para usar cout
+#include <stdlib.h> // para usar la funcion abs
+#include <stdio.h> // no necesitaria esta bibnioteca si usara cout en lugar de printf
 
 using namespace std;
 
