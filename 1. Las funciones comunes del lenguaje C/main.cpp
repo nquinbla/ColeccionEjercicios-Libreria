ad6cb82e6d8c25792931be98eb358e57ac280856
nquinbla <nquinbla@myuax.com>
2023-12-13 22:07:10 +0100

    no se que es esto

--- 1. Las funciones comunes del lenguaje C/main.cpp
+++ 1. Las funciones comunes del lenguaje C/main.cpp
@@ -2,3 +2,17 @@
 // b. Usa la función rand() para generar un número aleatorio. Luego, usa srand() para cambiar la semilla de la generación de números aleatorios y observa cómo cambian los números generados.
 
 #include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+    int numero = 33;
+
+    int abs_numero = abs(numero);
+
+    cout << "El valor absoluto de " << numero << " es " << abs_numero << endl;
+
+    return 0;
+}
