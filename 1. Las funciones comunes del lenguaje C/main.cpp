0dce2ab43e1be42406e3b8e7642d5ff2f51f3267
nquinbla <nquinbla@myuax.com>
2023-12-13 22:03:15 +0100

    a

---
+++ 1. Las funciones comunes del lenguaje C/main.cpp
@@ -0,0 +1,4 @@
+// a. Utiliza la función abs() para calcular el valor absoluto de un número entero. Prueba con varios números, incluyendo negativos.
+// b. Usa la función rand() para generar un número aleatorio. Luego, usa srand() para cambiar la semilla de la generación de números aleatorios y observa cómo cambian los números generados.
+
+#include <iostream>
