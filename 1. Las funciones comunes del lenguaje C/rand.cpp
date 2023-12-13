54f7f8e217809a8728a5d48fbcc5ea186119b662
nquinbla <nquinbla@myuax.com>
2023-12-13 22:34:11 +0100

    a

---
+++ 1. Las funciones comunes del lenguaje C/rand.cpp
@@ -0,0 +1,8 @@
+// b. Usa la función rand() para generar un número aleatorio. Luego, usa srand() para cambiar la semilla de la generación de números aleatorios y observa cómo cambian los números generados.
+
+#include <iostream> // para usar cout
+#include <stdlib.h> // para usar la funcion abs
+#include <time.h> // para usar la funcion time
+
+using namespace std;
+
