ef86aabcf2d6e20238ab56ba54b9eca8e3367dd4
nquinbla <nquinbla@myuax.com>
2023-12-13 22:41:22 +0100

    a

--- 2. Cadenas/strcmp.cpp
+++ 2. Cadenas/strcmp.cpp
@@ -6,3 +6,17 @@
 
 using namespace std;
 
+int main() {
+    char cadena1[] = "La tarta de queso";
+    char cadena2[] = "es la mejor";
+
+    int resultado = strcmp(cadena1, cadena2);
+
+    if (resultado == 0) {
+        cout << "Las cadenas son iguales" << endl;
+    } else {
+        cout << "Las cadenas son diferentes" << endl;
+    }
+
+    return 0;
+}
