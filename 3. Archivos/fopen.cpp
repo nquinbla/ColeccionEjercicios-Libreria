6b9c8c351dc5b3fe39eb6b7910ed9d833c0b461a
nquinbla <nquinbla@myuax.com>
2023-12-13 22:48:59 +0100

    la tarta de queso es la mejor

---
+++ 3. Archivos/fopen.cpp
@@ -0,0 +1,18 @@
+// a. Usa la función fopen() para abrir un archivo de texto. Si el archivo no existe, muestra un mensaje de error.
+
+#include <iostream> // para usar cout
+#include <stdio.h> // para usar fopen
+
+using namespace std;
+
+int main() {
+    FILE *archivo = fopen("archivo.txt", "r");
+
+    if (archivo == NULL) {
+        cout << "Error al abrir el archivo" << endl;
+    }
+
+    fclose(archivo);
+
+    return 0;
+}
