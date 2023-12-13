49f1452dc0dbc7ea8a9198a77af34179fe26ed0d
nquinbla <nquinbla@myuax.com>
2023-12-13 22:40:48 +0100

    d

---
+++ 2. Cadenas/strcmp.cpp
@@ -0,0 +1,8 @@
+// a. Crea dos cadenas de caracteres y usa la función strcmp() para compararlas. Prueba con cadenas iguales y diferentes.
+
+#include <iostream> // para usar cout
+#include <string.h> // para usar la funcion strcmp
+#include <stdio.h> // no necesitaria esta bibnioteca si usara cout en lugar de printf
+
+using namespace std;
+
