6e1bf23b739c7d2fff11b3abcdbceaeab7333ae2
nquinbla <nquinbla@myuax.com>
2023-12-13 22:38:21 +0100

    holicaracoli

--- 1. Las funciones comunes del lenguaje C/rand.cpp
+++ 1. Las funciones comunes del lenguaje C/rand.cpp
@@ -10,7 +10,7 @@ int main() {
     srand(time(0)); // cambia la semilla de la generacion de numeros aleatorios
 
     int numero_random = rand();
-    cout << "El numero aleatorio es " << numero_random << endl;
+    cout << "El numero aleatorio generado utilizando la funcion rand() es " << numero_random << endl;
 
     return 0;
 }
